4755fd62c0a57cfe1d1a77d04c373bca5bba7386
JoaoVitor-QA <joaovitor.santos@actuar.com>
2025-04-24 14:25:18 -0300

    version 3 prd

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+// svelte.config.js
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter({
+			pages: 'build', // Pasta onde o site estático será gerado
+			assets: 'build', // Onde os assets (JS/CSS) vão
+			fallback: 'index.html', // Ou '404.html' - importante para SvelteKit funcionar
+			precompress: false,
+			strict: true
+		}),
+		// ... talvez 'alias' ou outras configs ...
+	},
+	// ... talvez 'preprocess' ...
 };
-
 export default config;
